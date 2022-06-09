3556c70304582b7e589f5898327ad7dd51b6e120
Julio Neto <nettojulio@hotmail.com>
2022-06-08 21:04:29 -0300

    create static class for AWS Credentials

--- src/main/java/e8ilab2/apipedidos/services/SQSService.java
+++ src/main/java/e8ilab2/apipedidos/services/SQSService.java
@@ -8,31 +8,16 @@ import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
 import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
+import static e8ilab2.apipedidos.services.AWSCredentials.awsCredentialsDispatcher;
+
 public class SQSService {
     public static void sendMessage(String message){
-        AwsCredentialsProvider credentialsProvider = new AwsCredentialsProvider() {
-            @Override
-            public AwsCredentials resolveCredentials() {
-                return new AwsCredentials() {
-                    @Override
-                    public String accessKeyId() {
-                        return System.getenv("AWS_ACCESS_KEY");
-                    }
-
-                    @Override
-                    public String secretAccessKey() {
-                        return System.getenv("AWS_SECRET_KEY");
-                    }
-                };
-            }
-        };
 
         SqsClient sqsClient = SqsClient.builder()
                 .region(Region.US_EAST_1)
-                .credentialsProvider(credentialsProvider)
+                .credentialsProvider(awsCredentialsDispatcher())
                 .build();
 
-        // ===== Busca uma Fila =====
         GetQueueUrlRequest request = GetQueueUrlRequest.builder()
                 .queueName("sqs-e8-desafio2")
                 .queueOwnerAWSAccountId("755977887883").build();
