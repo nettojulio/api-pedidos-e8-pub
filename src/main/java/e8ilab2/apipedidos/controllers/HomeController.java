b0371b230416a0810183f04305cfc654b5643ebe
Julio Neto <nettojulio@hotmail.com>
2022-06-17 20:19:34 -0300

    Update HomeController.java

--- src/main/java/e8ilab2/apipedidos/controllers/HomeController.java
+++ src/main/java/e8ilab2/apipedidos/controllers/HomeController.java
@@ -2,10 +2,12 @@ package e8ilab2.apipedidos.controllers;
 
 import e8ilab2.apipedidos.utils.Messages;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class HomeController {
 
     @GetMapping("/")
